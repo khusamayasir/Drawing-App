b831febdf59902451508e7fd40234b886900b60a
Usama-Yasir <usamayasir32@gmail.com>
2022-03-22 04:15:19 +0500

    updated commit

--- script.js
+++ script.js
@@ -1,9 +1,14 @@
 const canvas = document.getElementById("canvas");
+const increaseBtn = document.getElementById("increase");
+const decreaseBtn = document.getElementById("decrease");
+const sizeEl = document.getElementById("size");
+const colorEl = document.getElementById("color");
 
 const ctx = canvas.getContext("2d");
 
 let size = 20;
 let isPressed = false;
+let color = "black";
 
 canvas.addEventListener("mousedown" , () => {
     isPressed = true;
@@ -25,9 +30,37 @@ canvas.addEventListener("mousemove", (e) => {
 function drawCircle(x, y) {
     ctx.beginPath();
     ctx.arc(x, y, size, 0, Math.PI * 2);
+    ctx.fillStyle = color;
     ctx.fill();
 }
 
+increaseBtn.addEventListener("click", () => {
+    size += 5;
+
+    if(size > 50) {
+        size = 50;
+    }
+
+    updateSizeOnScreen();
+});
+
+decreaseBtn.addEventListener("click", () => {
+    size -= 5;
+
+    if(size < 5) {
+        size = 5;
+    }
+
+    updateSizeOnScreen();
+});
+
+colorEl.addEventListener("change", (e) => {
+    color = e.target.value;
+});
+
+function updateSizeOnScreen() {
+    sizeEl.innerText = size;
+}
 // function draw() {
 //     ctx.clearRect(0 , 0, canvas.width, canvas.height);
 
