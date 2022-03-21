b1399e4bf914871bb1aaad2183648ff145d15e95
Usama-Yasir <usamayasir32@gmail.com>
2022-03-22 03:40:04 +0500

    updated commit

--- script.js
+++ script.js
@@ -2,13 +2,38 @@ const canvas = document.getElementById("canvas");
 
 const ctx = canvas.getContext("2d");
 
-let size = 30;
+let size = 20;
+let isPressed = false;
 
-canvas.addEventListener("mousedown", () => {
+canvas.addEventListener("mousedown" , () => {
+    isPressed = true;
+});
 
+canvas.addEventListener("mouseup" , () => {
+    isPressed = false;
+});
+
+canvas.addEventListener("mousemove", (e) => {
+    if(isPressed) {
+        const x = e.offsetX;
+        const y = e.offsetY; 
+    
+        drawCircle(x, y);
+    }
 });
 
 function drawCircle(x, y) {
     ctx.beginPath();
     ctx.arc(x, y, size, 0, Math.PI * 2);
+    ctx.fill();
 }
+
+// function draw() {
+//     ctx.clearRect(0 , 0, canvas.width, canvas.height);
+
+//     drawCircle(x, y);
+
+//     requestAnimationFrame(draw);
+// }
+
+// draw();
